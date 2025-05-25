ca6cbc5d4cb06c094c94709d9f2e8ed52e9731cf
erandesamadhan2003 <erandesamadhan2003@gmail.com>
2025-05-25 15:46:58 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -4,8 +4,22 @@ import net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+    socket.on("data", (data) => {
+        //  request data of the client
+        console.log(data.toString());
+
+        // Write the HTTP 200 OK respone
+        // HTTP/1.1 version of the http protocol
+        // 200 ok the status code and the status message. 200 means the success
+        // \r\n\r\n — a blank line to separate headers from the body writtern inside the \r\n `Actual content` \r\n
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
+        socket.end()
+    })
+    socket.on("close", () => {
+        socket.end();
+    });
 });
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+    console.log('server is listening to the port 4221')
+});
